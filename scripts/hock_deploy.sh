b45ef63e99206f28317e0071f99000bb44014390
mortr <mortr@yandex.ru>
2018-01-28 20:42:31 +0300

    test hock integration

---
+++ scripts/hock_deploy.sh
@@ -0,0 +1,6 @@
+curl \
+-F "status=2" \
+-F "notify=0" \
+-F "ipa=@app/build/outputs/apk/release/app-release.apk" \
+-H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
+https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload
